23816fb221ee804ce6653b789e4d5fdcd0fb43b0
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-01-07 06:54:07 +0000

    Task 4: Binary tree leaf commit

--- test/binary_trees.h
+++ test/binary_trees.h
@@ -1,6 +1,9 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+/* import libararies */
+#include <stddef.h>
+#include <stdlib.h>
 /**
  * struct binary_tree_s - Binary tree node.
  *
@@ -28,6 +31,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 /* delete an entire binary tree prototype */
 void binary_tree_delete(binary_tree_t *tree);
+/* checks if a node is a leaf prototype */
+int binary_tree_is_leaf(const binary_tree_t *node);
 /* print binary tree prototype */
 void binary_tree_print(const binary_tree_t *);
 #endif /* BINARY_TREES_H */
