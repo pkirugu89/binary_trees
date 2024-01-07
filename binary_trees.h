d69f50865b090a72d51f6ece287567d26c822826
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-01-07 10:12:41 +0000

    Task 12: Binary tree leaves count commit

--- binary_trees.h
+++ binary_trees.h
@@ -47,6 +47,8 @@ size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 /* binary tree size prototype */
 size_t binary_tree_size(const binary_tree_t *tree);
+/* Leaves count in binary tree prototype */
+size_t binary_tree_leaves(const binary_tree_t *tree);
 /* print binary tree prototype */
 void binary_tree_print(const binary_tree_t *);
 #endif /* BINARY_TREES_H */
