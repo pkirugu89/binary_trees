ddafac07bc7751e75fc86ca862fb618b53a8c889
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-01-07 06:33:45 +0000

    Task 3: delete binary tree commit

--- binary_trees.h
+++ binary_trees.h
@@ -26,6 +26,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 /* inserts a node as the right-child of another node prototype */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+/* delete an entire binary tree prototype */
+void binary_tree_delete(binary_tree_t *tree);
 /* print binary tree prototype */
 void binary_tree_print(const binary_tree_t *);
 #endif /* BINARY_TREES_H */
---
+++ test/3-binary_tree_delete.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	/* Recursive deletion of left and right subtrees */
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	/* Delete the current node */
+	free(tree);
+}
+
