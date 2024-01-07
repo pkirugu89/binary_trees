6ee7259f8d7a51016187dd802f11fa3fb06a47a8
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-01-07 12:34:49 +0000

    Task 14: Binary tree balancing factor commit

--- test/14-binary_tree_balance.c
+++ test/14-binary_tree_balance.c
@@ -18,4 +18,22 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (left_height - right_height);
 }
+/**
+ * binary_tree_height - Measures binary tree's height.
+ * @tree: Pointer to the root node of the tree to measure.
+ *
+ * Return: Height of the binary tree, 0 if the tree is null.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree)
+	{
+		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((left_height > right_height) ? left_height : right_height);
+	}
+	return (0);
+}
 
